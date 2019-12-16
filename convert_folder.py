df863e0e0be64dcf3ad77bc513232e4a15daca83
Manlin Zhang <40782767+mzhan129@users.noreply.github.com>
2019-12-16 15:39:52 -0700

    first 10 sec

--- convert_folder.py
+++ convert_folder.py
@@ -11,7 +11,7 @@ def convert_folder(folder_path):
     videos = [y for x in list(os.walk(folder_path)) for y in x[2]]
     videos = [x for x in videos if '.mp4' in x]
     # Create a mapping for the videos
-    video_mapping = pr
+    video_mapping = {}
     for i, v in enumerate(videos):
         video_mapping[v] = i
     encoded_mappings = encoder.encode(video_mapping)
@@ -23,6 +23,7 @@ def convert_folder(folder_path):
 
     # Convert the videos
     for video in videos:
+        print(video)
         results_path = os.path.join(folder_path, str(video_mapping[video]))
         os.mkdir(results_path)
         subprocess.run([
@@ -30,7 +31,11 @@ def convert_folder(folder_path):
             '-i',
             os.path.join(folder_path, video),
             '-r',
-            '10/60',
+            '30/60',
+            '-ss',
+            '00:00:00',
+            '-t',
+            '10',
             os.path.join(results_path,'%06d.jpg'), '-hide_banner'])
 
 
